3ab3bfa34670ef9ad410f8d9452f7641df9463b8
Charles Moscofian <charles.moscofian@mercadolivre.com>
2020-07-16 13:21:13 -0300

    Create a status handler on rest service; If a response has a status code different than 2xx, try to parse error message if not possible, simply uses the response body.

--- src/service/rest.go
+++ src/service/rest.go
@@ -2,11 +2,13 @@ package service
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"time"
 
+	"github.com/cmoscofian/meliponto/src/model"
 	"github.com/cmoscofian/meliponto/src/util/constants"
 )
 
@@ -45,28 +47,48 @@ func Post(uri string, headers map[string]string, body []byte, chbs chan<- []byte
 		return
 	}
 
+	if resp.StatusCode/100 != 2 {
+		er := new(model.ErrorResponse)
+		message := string(bs)
+		if err := json.Unmarshal(bs, er); err == nil {
+			message = er.Message
+		}
+		cher <- fmt.Errorf(constants.RestServiceError, http.StatusText(resp.StatusCode), resp.StatusCode, message)
+		return
+	}
+
 	chbs <- bs
 }
 
-func Get(uri string, headers map[string]string, ch chan<- []byte) {
+func Get(uri string, headers map[string]string, chbs chan<- []byte, cher chan<- error) {
 	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s%s", constants.BaseURI, uri), nil)
 	if err != nil {
-		ch <- []byte(err.Error())
+		cher <- err
 		return
 	}
 
 	resp, err := client.Do(req)
 	if err != nil {
-		ch <- []byte(err.Error())
+		cher <- err
 		return
 	}
 	defer resp.Body.Close()
 
 	bs, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		ch <- []byte(err.Error())
+		cher <- err
 		return
 	}
 
-	ch <- bs
+	if resp.StatusCode/100 != 2 {
+		er := new(model.ErrorResponse)
+		message := string(bs)
+		if err := json.Unmarshal(bs, er); err == nil {
+			message = er.Message
+		}
+		cher <- fmt.Errorf(constants.RestServiceError, http.StatusText(resp.StatusCode), resp.StatusCode, message)
+		return
+	}
+
+	chbs <- bs
 }
